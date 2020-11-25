57b8adf401f5162f06dce8adc8d734af0bdf8178
matija-mijalkovic <matija.mijalkovic@gmail.com>
2020-11-25 21:15:28 +0100

    Integration test for survey results

--- Care-CureHospital/WebAppPatientTests/Integration/QuestionAnswersTest.cs
+++ Care-CureHospital/WebAppPatientTests/Integration/QuestionAnswersTest.cs
@@ -1,20 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using WebAppPatient;
 using Xunit;
 
 namespace WebAppPatientTests.Integration
 {
-    public class QuestionAnswersTest
+    public class QuestionAnswersTest : IClassFixture<WebApplicationFactory<Startup>>
     {
 
-       /* private readonly WebApplicationFactory<>
+        private readonly WebApplicationFactory<Startup> factory;
 
-        [Fact]
-        public void Get_survey_results()
+        public QuestionAnswersTest(WebApplicationFactory<Startup> factory)
         {
-
+            this.factory = factory;
         }
-       */
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public async void Get_survey_results(HttpStatusCode expectedAnswer)
+        {
+            HttpClient client = factory.CreateClient();
+
+            HttpResponseMessage response = await client.GetAsync("/api/survey/getSurveyResults");
+
+            response.StatusCode.ShouldBeEquivalentTo(expectedAnswer);
+        }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] {  HttpStatusCode.OK }
+        };
+
     }
 }
