4f7f38c862384a1e6213db65ce47ab85315a717f
jelenajelena11 <stojanovicj97@gmail.com>
2020-12-04 23:39:13 +0100

    Added server side

---
+++ Care-CureHospital/GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using IntegrationAdapters.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:51492");
+            var client = new NetGrpcService.NetGrpcServiceClient(channel);
+            var requested = new MedicationRequest { Name = "Aspirin" };
+            var reply = await client.transferAsync(requested);
+            Console.WriteLine($"{reply.Message}");
+            Console.ReadLine();
+        }
+    }
+}
--- Care-CureHospital/IntegrationAdapters/Startup.cs
+++ Care-CureHospital/IntegrationAdapters/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Web.Http.Cors;
+using Microsoft.AspNetCore.Http;
 
 namespace IntegrationAdapters
 {
@@ -29,15 +30,22 @@ namespace IntegrationAdapters
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
             services.AddControllers();
             services.AddControllersWithViews()
                 .AddNewtonsoftJson(options =>
                      options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-            services.AddCors();
+            services.AddGrpc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -48,14 +56,39 @@ namespace IntegrationAdapters
 
             app.UseRouting();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGrpcService<NetGrpcServiceImpl>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("");
+                });
             });
 
-            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true));
+            /*    server = new Server()
+                {
+                    Services = { NetGrpcService.BindService(new NetGrpcServiceImpl()) },
+                    Ports = { new ServerPort("localhost", 4111, ServerCredentials.Insecure) }
+                };
+                server.Start();
+
+                applicationLifetime.ApplicationStopping.Register(OnShutDown);*/
         }
+
+        /*   private void OnShutDown()
+           {
+               if(server != null)
+               {
+                   server.ShutdownAsync().Wait();
+               }
+           }*/
     }
+
+
 }
