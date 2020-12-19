9f2dcecec8d854e297b7c6c1f08c9809104954e1
Aca Simic <acasimic98@uns.ac.rs>
2020-12-19 20:28:42 +0100

    Created dockerFiles for test

--- Care-CureHospital/IntegrationTests/WebAppPatientIntegrationTests/MedicalExaminationReportTestsIntegration.cs
+++ Care-CureHospital/IntegrationTests/WebAppPatientIntegrationTests/MedicalExaminationReportTestsIntegration.cs
@@ -19,22 +19,22 @@ namespace IntegrationTests.WebAppPatientIntegrationTests
             this.factory = factory;
         }
 
-        //[Theory]
-        //[MemberData(nameof(MedicalExaminationReportData))]
-        //public async void Find_Reports_With_Doctor_Searh_Parameter(HttpStatusCode expectedResponseStatusCode)
-        //{
-        //    HttpClient client = factory.CreateClient();
+        [Theory]
+        [MemberData(nameof(MedicalExaminationReportData))]
+        public async void Find_Reports_With_Doctor_Searh_Parameter(HttpStatusCode expectedResponseStatusCode)
+        {
+            HttpClient client = factory.CreateClient();
 
-        //    HttpResponseMessage response = await client.GetAsync("/api/medicalExaminationReport/findReportsByDoctor?patientId=1&doctor=Aleksandar");
+            HttpResponseMessage response = await client.GetAsync("/api/medicalExaminationReport/findReportsByDoctor?patientId=1&doctor=Aleksandar");
 
-        //    response.StatusCode.ShouldBeEquivalentTo(expectedResponseStatusCode);
-        //}
+            response.StatusCode.ShouldBeEquivalentTo(expectedResponseStatusCode);
+        }
 
-        //public static IEnumerable<object[]> MedicalExaminationReportData()
-        //{
-        //    var retVal = new List<object[]>();
-        //    retVal.Add(new object[] { HttpStatusCode.OK });
-        //    return retVal;
-        //}
+        public static IEnumerable<object[]> MedicalExaminationReportData()
+        {
+            var retVal = new List<object[]>();
+            retVal.Add(new object[] { HttpStatusCode.OK });
+            return retVal;
+        }
     }
 }
