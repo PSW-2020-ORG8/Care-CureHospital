959e310e17e9b35bb31e1ffad2be783a176de119
Stefan Beljic <stefan.beljic23@gmail.com>
2021-01-07 21:34:33 +0100

    Added Advertisement in FeedbackMicroservice

---
+++ Care-CureHospital/FeedbackMicroservice/Controllers/AdvertisementController.cs
@@ -0,0 +1,28 @@
+﻿using FeedbackMicroservice.Domain;
+using FeedbackMicroservice.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FeedbackMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvertisementController : ControllerBase
+    {
+        private IAdvertisementService advertisementService;
+
+        public AdvertisementController(IAdvertisementService advertisementService) 
+        {
+            this.advertisementService = advertisementService;
+        }
+
+        [HttpGet]       // GET /api/advertisement
+        public IActionResult GetAllAdvertisements()
+        {
+            List<Advertisement> result = new List<Advertisement>();
+            advertisementService.GetAllEntities().ToList().ForEach(advertisement => result.Add(advertisement));
+            return Ok(result);
+        }
+    }
+}
---
+++ Care-CureHospital/FeedbackMicroservice/Service/IAdvertisementService.cs
@@ -0,0 +1,12 @@
+﻿using FeedbackMicroservice.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedbackMicroservice.Service
+{
+    public interface IAdvertisementService : IService<Advertisement, int>
+    {      
+    }
+}
