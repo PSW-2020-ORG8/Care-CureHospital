4909484739375d3cb6de5f72604863b9c61252ec
Aca Simic <acasimic98@uns.ac.rs>
2021-01-15 18:51:22 +0100

    Created databases scripts

---
+++ Care-CureHospital/EventSourcingMicroservice/init.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE `LoginEvents` (
+    `Id` char(36) NOT NULL,
+    `TimeStamp` datetime(6) NOT NULL,
+    `UserName` longtext CHARACTER SET utf8mb4 NULL,
+    `UserId` int NOT NULL,
+    CONSTRAINT `PK_LoginEvents` PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `URLEvents` (
+    `Id` char(36) NOT NULL,
+    `TimeStamp` datetime(6) NOT NULL,
+    `Url` longtext CHARACTER SET utf8mb4 NULL,
+    `Method` longtext CHARACTER SET utf8mb4 NULL,
+    CONSTRAINT `PK_URLEvents` PRIMARY KEY (`Id`)
+);
+
